1c9ed3fe78c981f934e52ce2bd6f0ede134248db
dsmv04 <dalton.munoz.v@hotmail.com>
2023-10-11 19:30:30 -0300

    Asignacion 3 dia 2 semana 6

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// Create an empty list of type object
+List<object> things = new List<object>();
+
+// Add the following values to the list: 7, 28, -1, true, "chair"
+things.Add(7);
+things.Add(28);
+things.Add(-1);
+things.Add(true);
+things.Add("chair");
+
+int sum = 0;
+
+// Loop through the list and print all values (Hint: Type Inference might help here!)
+foreach (var thing in things)
+{
+    Console.WriteLine(thing);
+
+    // Add all values that are Int type together and output the sum
+    if (thing is int)
+    {
+        sum += (int)thing;
+    }
+
+}
+
+Console.WriteLine(sum);
